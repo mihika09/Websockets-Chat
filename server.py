535736bf5bf9a27b56a150377e5e4f68f03314ac
Mihika <mohta.mihika@gmail.com>
2019-04-24 10:54:24 +0530

    Revert "Changes to UI"

--- server.py
+++ server.py
@@ -18,23 +18,21 @@ async def send_message(data, websocket):
 		await asyncio.wait([user.send(msg) for user in ROOMS[data['room_id']]])
 
 
-async def notify_user(username, room_id):
-	user_notification = json.dumps({'type': 'user', 'count': len(ROOMS[room_id]), 'name': username, 'room_id': room_id})
+async def notify_user(msg, room_id):
+	user_notification = json.dumps({'type': 'user', 'count': len(ROOMS[room_id]), 'notification': msg, 'room_id': room_id})
 	if ROOMS[room_id]:
 		await asyncio.wait([user.send(user_notification) for user in ROOMS[room_id]])
 
 
 async def register(data, websocket):
-	print("#################")
 	id = uuid.uuid1()
 	USERS[websocket] = {'id': id, 'name': data['username'], 'room_id': data['room_id']}
 	if data['room_id'] not in ROOMS:
 		ROOMS[data['room_id']] = []
 	ROOMS[data['room_id']].append(websocket)
+	msg = '{} has joined the chat'.format(USERS[websocket]['name'])
 
-	print("ROOMS: ", ROOMS)
-
-	await notify_user(data['username'], data['room_id'])
+	await notify_user(msg, data['room_id'])
 
 
 async def unregister(websocket):
@@ -47,7 +45,6 @@ async def unregister(websocket):
 
 async def chat(websocket, path):
 	notif = await websocket.recv()
-	print("Notification: ", notif)
 	data = json.loads(notif)
 	print("Received username: ", data)
 	await register(data, websocket)
@@ -68,7 +65,7 @@ async def chat(websocket, path):
 
 if __name__ == '__main__':
 
-	start_server = websockets.serve(chat, 'localhost', 8080)
+	start_server = websockets.serve(chat, '192.168.43.91', 8080)
 
 	loop = asyncio.get_event_loop()
 	server = loop.run_until_complete(start_server)
